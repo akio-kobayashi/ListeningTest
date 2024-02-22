faf597862ee403573955a3da507216f261f9a2f9
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2024-02-22 17:11:36 +0900

    fix bugs

--- listening_test.py
+++ listening_test.py
@@ -34,16 +34,106 @@ class DataProvider(object):
 
 trial_data_provider = None
 test_data_provider = None
+speaker_data_provider = None
 eval_dict = {'eval':[], 'key': [], 'value': [], 'start_time': [], 'end_time': [], 'path': []}
 
 class TopView(ft.View):
     def __init__(self):
-        global trial_data_provider
-
+        global test_data_provider
         controls = [
-            ft.Text('これから聴取実験を行います'),
-            ft.Text('まず，評価方法を確認するために'+ str(trial_data_provider.remind()) +'個のサンプルを使って試行します．'),
-            ft.Text('「次へ」をクリックして進みます'),
+            ft.Text(spans=[
+                    ft.TextSpan(
+                        'これから聴取実験を行います。',
+                    )
+                ]
+            ),
+            ft.Text("　"),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '実験では、複数の人々が会話している音声がスピーカーから流れます。　　　　　　　　　',
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '実験参加者のあなたは、この後で紹介する聞き取り対象の話者の音声を聞き取ります。　　',
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '聞き取り音声は全部で',
+                    ),
+                    ft.TextSpan(
+                        str(test_data_provider.__length__()),
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    ),
+                    ft.TextSpan(
+                        'あります。　　　　　　　　　　　　　　　　　　　　　　　　',
+                    )
+                ]
+            ),
+            ft.Text("　"),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '聞き取りに要した労力にあわせて、次の',
+                    ),
+                    ft.TextSpan(
+                        '５つ',
+                        ft.TextStyle(size=14, weight=ft.FontWeight.BOLD)
+                    ),
+                    ft.TextSpan(
+                        'の選択肢からひとつを選んで答えてください。',
+                    )
+                ]
+            ),
+            ft.Text("　"),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '「完全にリラックスして聴取できる」　　　　',
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '「注意を払う必要はあるが特別な努力は不要」',
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '「ある程度の努力が必要」　　　　　　　　　',
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '「かなりの努力が必要」　　　　　　　　　　',
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '「どんな努力をしても意味は理解できない」　',
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    )
+                ]
+            ),
+            ft.Text('　'),
+            ft.Text('準備ができたら「次へ」をクリックして進みます。'),
             ft.ElevatedButton(content=ft.Text('次へ'), on_click=self.clicked)
         ]
         super().__init__("/", controls=controls)
@@ -51,13 +141,150 @@ class TopView(ft.View):
         self.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     def clicked(self, e):
-        e.page.go("/trialview")
+        e.page.go("/speakerview")
+
+class SpeakerView(ft.View):
+    def __init__(self, key, path):
+
+        self.audio = ft.Audio(src=path, volume=1, balance=0, 
+                         on_loaded=self.activate_play_button,
+                         on_state_changed=self.state_changed,
+                         autoplay=False)
+        
+        self.playb = ft.ElevatedButton(content=ft.Text("再生"), 
+                                       on_click=lambda _: self.audio.play(), 
+                                       disabled=False)
+        self.playb.padding = ft.padding.only(top=50, bottom=200)
+
+        self.nextb = ft.ElevatedButton(content=ft.Text('次へ'), 
+                                       on_click=self.clicked,
+                                       disabled=True)
+        #self.nextb.padding = ft.padding.only(top=100)
+        
+        global spekaer_data_provider
+        controls = [
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '聞き取りの対象となる話者は'
+                    ),
+                    ft.TextSpan(
+                        str(speaker_data_provider.__length__()),
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    ),
+                    ft.TextSpan(
+                        '人になります。'
+                    )
+                ]
+            ),
+            ft.Text('　'),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '下にある「再生」をクリックして'
+                    ),
+                    ft.TextSpan(
+                        str(speaker_data_provider.counter),
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    ),
+                    ft.TextSpan(
+                        '人目の話者の音声を聞き取って、声の特徴を覚えてください。'
+                    ),
+                ]
+            ),
+            ft.Text('覚えるまで再生は繰り返しても構いません。'),
+            ft.Text("　"),
+            self.playb,
+            self.audio,
+            ft.Text("　"),
+            ft.Text("　"),
+            ft.Text('覚えたら次へ進みます。'),
+            self.nextb
+        ]
+
+        super(SpeakerView, self).__init__("/speakerview", controls=controls)
+        self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        self.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    def clicked(self, e):
+        global speaker_data_provider
+        try:
+            key, path = speaker_data_provider.__next__()
+            e.page.views.pop()
+            e.page.views.append(SpeakerView(key, path))
+            e.page.update()
+        except StopIteration:
+            e.page.go('/intermview')
+
+    def activate_play_button(self, e):
+        self.playb.disabled=False
+        self.playb.update()
+    
+    def state_changed(self, e):
+        if e.data == 'completed':
+            self.playb.disabled=False
+            self.playb.update()
+            self.nextb.disabled=False
+            self.nextb.update()
+        elif e.data == 'playing':
+            self.playb.disabled=True
+            self.playb.update()
+            self.nextb.disabled=True
+            self.nextb.update()
 
 class IntermView(ft.View):
     def __init__(self):
+        global speaker_data_provider
         controls = [
-            ft.Text('次から本番になります．'),
-            ft.Text('準備ができたら「次へ」をクリックして始めます'),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '次から本番になります。'
+                    )
+                ]
+            ),
+            ft.Text('　'),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '音声は複数の人の声が混じりあって再生されますが、　　　　　　　　　　　　　　　'
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        str(speaker_data_provider.__length__()),
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD),
+                    ),
+                    ft.TextSpan(
+                        '人のうちの誰かひとり（対象者）の声が必ず入っています。　　　　　　　　　　　'
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '対象者の声の聞き取りに関して、実験のはじめに説明した５段階で評価してください。'
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '聞き取りは'
+                    ),
+                    ft.TextSpan(
+                        '１回しかできませんので',
+                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
+                    ),
+                    ft.TextSpan(
+                        '、注意してください。　　　　　　　　　　　'
+                    )
+                ]
+            ),
+            ft.Text('　'),
+            ft.Text('準備ができたら「次へ」をクリックして始めます。'),
             ft.ElevatedButton(content=ft.Text('次へ'), on_click=self.clicked)
         ]
         super().__init__("/", controls=controls)
@@ -83,13 +310,13 @@ class LastView(ft.View):
 
 class TestViewBase(ft.View):
     def __init__(self, key, path):
-        self.play_inst = ft.Text('再生ボタンを押して音声を再生してください')
+        self.play_inst = ft.Text('「再生」をクリックして音声を聞き取ってください。')
         self.playb = ft.ElevatedButton(content=ft.Text("再生"), 
                                        on_click=lambda _: self.audio.play(), 
                                        disabled=True)
         self.playb.padding = ft.padding.only(top=50, bottom=100)        
         self.audio, self.opinion = self.setup(path)
-        self.inst = ft.Text('いま聞き取った音声について，以下の5段階で評価してください', 
+        self.inst = ft.Text('対象者の音声の聞き取りに関して以下の5段階で評価してください。', 
                             color=ft.colors.WHITE)
         self.inst.padding=ft.padding.only(top=200)
         #self.opinion.padding = ft.padding.only(left=1024, bottom=50)
@@ -98,6 +325,7 @@ class TestViewBase(ft.View):
                                        on_click=self.clicked,
                                        disabled=True)
         self.nextb.padding = ft.padding.only(top=100)
+        self.nextm = ft.Text('評価が終わったら「次へ」をクリックして進みます。', color=ft.colors.WHITE)
 
         self.key = key
         self.path = path
@@ -150,7 +378,6 @@ class TestView(TestViewBase):
         global test_data_provider
         self.q_number = ft.Text("No." + str(test_data_provider.counter) + " of " + str(test_data_provider.__length__()))
         self.q_number.padding = ft.padding.only(bottom=100)
-        #self.reminder = ft.Text("残り：" + str(test_data_provider.remind()) + "/" + str(test_data_provider.__length__()))
 
         controls = [
             self.q_number,
@@ -160,6 +387,7 @@ class TestView(TestViewBase):
             self.inst,
             self.opinion,
             #self.reminder,
+            self.nextm,
             self.nextb
         ]
 
@@ -173,10 +401,11 @@ class TestView(TestViewBase):
         if e.data == 'completed':
             self.inst.color=ft.colors.BLACK
             self.inst.update()
+            global jst_standard
+            self.start_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
+        elif e.data == 'playing':
             self.playb.disabled=True
             self.playb.update()
-            global jst_standard
-            self.start_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S')
 
     def clicked(self, e):
         global test_data_provider, eval_dict
@@ -199,8 +428,10 @@ class TestView(TestViewBase):
         self.value = e.data
         self.nextb.disabled=False
         self.nextb.update()
+        self.nextm.color=ft.colors.BLACK
+        self.nextm.update()
         global jst_standard
-        self.end_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S')
+        self.end_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
     
 class TrialView(TestViewBase):
     def __init__(self, key, path):
@@ -242,10 +473,11 @@ class TrialView(TestViewBase):
             self.inst.color=ft.colors.BLACK
             self.add_inst2.update()
             self.inst.update()
-            self.playb.disabled=True
-            self.playb.update()
             global jst_standard
             self.start_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S')
+        elif e.data == 'playing':
+            self.playb.disabled=True
+            self.playb.update()
 
     def clicked(self, e):
         global trial_data_provider, eval_dict
@@ -275,16 +507,17 @@ class TrialView(TestViewBase):
 def main(page: ft.Page):
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_csv', type=str, required=True)
-    parser.add_argument('--trial_csv', type=str, required=True)
+    parser.add_argument('--speaker_csv', type=str, required=True)
     parser.add_argument('--output_csv', type=str, default="output.csv")
 
     args=parser.parse_args()
 
-    global test_data_provider, trial_data_provider
+    global test_data_provider, speaker_data_provider
     test_data_provider = DataProvider(args.test_csv, random=True)
-    trial_data_provider = DataProvider(args.trial_csv)
+    #trial_data_provider = DataProvider(args.trial_csv)
+    speaker_data_provider = DataProvider(args.speaker_csv)
 
-    page.title="sample"
+    page.title="ITU-T 800 Listening Effort"
 
     pop_flag = False
 
@@ -300,9 +533,9 @@ def main(page: ft.Page):
             elif page.route == '/testview':
                 key, path = test_data_provider.__next__()
                 page.views.append(TestView(key, path)) 
-            elif page.route == '/trialview':
-                key, path = trial_data_provider.__next__()
-                page.views.append(TrialView(key, path))
+            elif page.route == '/speakerview':
+                key, path = speaker_data_provider.__next__()
+                page.views.append(SpeakerView(key, path))
             elif page.route == '/intermview':
                 page.views.append(IntermView())
             elif page.route == '/lastview':
@@ -315,7 +548,7 @@ def main(page: ft.Page):
         top_view = page.views[-1]
         page.go(top_view.route)
     
-    page.window_full_screen = True
+    #page.window_full_screen = True
     page.on_route_change = route_change
     page.on_view_pop = view_pop
     page.views.clear()
