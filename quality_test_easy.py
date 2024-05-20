86247be4f658748fec5027635c3f6cc33b96dfd7
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2024-05-20 17:30:50 +0900

    add file

--- quality_test_easy.py
+++ quality_test_easy.py
@@ -3,12 +3,6 @@ import pandas as pd
 import argparse
 import time, datetime
 
-message0=['1: よくわかる', '2: ときどきわからない語がある', 
-          '3: 聞き手が話題を知っていればわかる', '4: ときどきわかる語がある', 
-          '5: 全くの了解不能']
-message1=['1: 全くの自然である（不自然な要素がない）', '2: やや不自然な要素がある', '3: 明らかに不自然である', '4: 顕著に不自然である', '5: 全く不自然である（自然な要素がない）']
-message2=['-2: 速い', '-1: やや速い', '0: 自然な速さである', '1: ややゆっくりしている', '2: ゆっくりしている',]
-
 # theme_style=ft.TextThemeStyle.DISPLAY_LARGE
 jst_standard = datetime.timezone(datetime.timedelta(hours=9), 'JST')
 
@@ -41,81 +35,7 @@ class DataProvider(object):
 trial_data_provider = None
 test_data_provider = None
 #speaker_data_provider = None
-eval_dict = {'eval':[], 'key': [], 'intelligiblity': [], 'loudness': [], 'intonation': [], 'accent': [], 'start_time': [], 'end_time': [], 'path': []}
-
-class SliderComponent(ft.UserControl):
-
-    def __init__(self, title, message, min=1.0, max=5.0, value=1.0, divisions=8):
-        super().__init__()
-        self.title = ft.Text(
-            spans=[
-                ft.TextSpan(title,
-                            ft.TextStyle(size=16, weight=ft.FontWeight.BOLD))
-            ]
-        )
-        self.message = message
-        self.divisions=divisions
-        self.min=min
-        self.max=max
-        self.value=value
-
-    def build(self):
-        self.slider_selected_value = self.value
-        self.slider_status = ft.Text(self.get_message(self.slider_selected_value))
-
-        return ft.Container(
-            ft.Column([
-                self.title,
-                ft.Slider(
-                    min=self.min,
-                    max=self.max,
-                    value=self.value,
-                    adaptive=False,
-                    divisions=self.divisions,
-                    active_color=ft.colors.BLUE,
-                    thumb_color=ft.colors.BLUE,
-                    on_change_start=self.handle_change_start,
-                    on_change_end=self.handle_change_end,
-                    on_change = self.handle_change,
-                    round=1,
-                    label='{value}'
-                ),
-                self.slider_status
-            ])
-        )
-
-    def get_message(self, value):
-        if self.divisions == 8:
-            return self.message[int(value * 2)-2]
-        elif self.divisions == 6:
-            return self.message[int(value) + 3]
-        else:
-            return self.message[int(value - 1)]
-        
-        
-    def handle_change_start(self, e):
-        try:
-            self.slider_status.value = self.get_message(e.control.value)
-            self.slider_selected_value = e.control.value
-            self.slider_status.update()
-        except:
-            pass
-        #print(self.slider_selected_value)
-
-    def handle_change_end(self, e):
-        self.slider_status.value = self.get_message(e.control.value)
-        self.slider_selected_value = e.control.value
-        self.slider_status.update()
-        #print(self.slider_selected_value)
-
-    def handle_change(self, e):
-        self.slider_status.value = self.get_message(e.control.value)
-        self.slider_selected_value = e.control.value
-        self.slider_status.update()
-        #print(self.slider_selected_value)
-
-    def get_value(self):
-        return self.value
+eval_dict = {'eval':[], 'key': [], 'value1': [], 'value2': [], 'start_time': [], 'end_time1': [], 'end_time2': [], 'path': []}
 
 class TopView(ft.View):
     def __init__(self):
@@ -131,7 +51,14 @@ class TopView(ft.View):
             ft.Text(
                 spans=[
                     ft.TextSpan(
-                        '実験では、さまざまな個人の音声がスピーカーから流れます。　　　　　　　　　',
+                        '実験では、さまざまな人が文章を読み上げた音声がスピーカーから流れます。',
+                    )
+                ]
+            ),
+            ft.Text(
+                spans=[
+                    ft.TextSpan(
+                        '実験参加者のあなたは、聞き取った音声の明瞭度と自然さを評価します。　　',
                     )
                 ]
             ),
@@ -145,52 +72,11 @@ class TopView(ft.View):
                         ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
                     ),
                     ft.TextSpan(
-                        'あります。　　　　　　　　　　　　　　　　　　　　　',
+                        '文あります。',
                     )
                 ]
             ),
             ft.Text("　"),
-            ft.Text(
-                spans=[
-                    ft.TextSpan(
-                        '聞き取った音声について，明瞭性，声の大きさの変化，抑揚，アクセントの観点から評価してください',
-                    ),
-                ]
-            ),
-            ft.Text("　"),
-            ft.Text(
-                spans=[
-                    ft.TextSpan(
-                        '明瞭性：音声の内容について「よくわかる」から「全くの了解不能」まで５段階で評価）　　　　　　　　',
-                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
-                    )
-                ]
-            ),
-            ft.Text(
-                spans=[
-                    ft.TextSpan(
-                        '声の大きさ：声の大きさの変化について「全くの自然である」から「全く不自然である」まで５段階で評価',
-                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
-                    )
-                ]
-            ),
-            ft.Text(
-                spans=[
-                    ft.TextSpan(
-                        '抑揚：全体の音の高低について，「全くの自然である」から「全く不自然である」まで５段階で評価　　　',
-                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
-                    )
-                ]
-            ),
-            ft.Text(
-                spans=[
-                    ft.TextSpan(
-                        'アクセント：単語の音の高低について「全くの自然である」から「全く不自然である」まで５段階で評価　',
-                        ft.TextStyle(size=16, weight=ft.FontWeight.BOLD)
-                    )
-                ]
-            ),
-            ft.Text('　'),
             ft.Text('準備ができたら「次へ」をクリックして進みます。'),
             ft.ElevatedButton(content=ft.Text('次へ'), on_click=self.clicked)
         ]
@@ -252,29 +138,23 @@ class LastView(ft.View):
         output_df = pd.DataFrame.from_dict(eval_dict, orient='columns')
         output_df.to_csv(path, index=False)
 
-class ViewBase(ft.View):
+class TestViewBase(ft.View):
     def __init__(self, key, path):
         self.play_inst = ft.Text('「再生」をクリックして音声を聞き取ってください。')
-        self.audio = ft.Audio(src=path, volume=1, balance=0, 
-                         on_state_changed=self.state_changed,
-                         on_loaded=self.activate_play_button,
-                         autoplay=False)
         self.playb = ft.ElevatedButton(content=ft.Text("再生"), 
                                        on_click=lambda _: self.audio.play(), 
                                        disabled=True)
         self.playb.padding = ft.padding.only(top=50, bottom=100)        
-
-        #self.inst = ft.Text('対象者の音声の聞き取りに関して以下の5段階で評価してください。', 
-        #                    color=ft.colors.WHITE)
-        #self.inst.padding=ft.padding.only(top=200)
+        self.audio, self.opinion1, self.opinion2 = self.setup(path)
+        self.inst1 = ft.Text('明瞭度に関して次の5段階で評価してください。', 
+                            color=ft.colors.GREY_50)
+        self.inst1.padding=ft.padding.only(top=50)
         #self.opinion.padding = ft.padding.only(left=1024, bottom=50)
-
-        self.component1 = SliderComponent('明瞭度', message0, min=1, max=5, divisions=4)
-        self.component2 = SliderComponent('声の大きさ', message1, min=1, max=5, divisions=4)
-        self.component3 = SliderComponent('抑揚（全体の音の高低）', message1, min=1, max=5, divisions=4) 
-        self.component4 = SliderComponent('アクセント（単語レベルの音の高低）', message1, min=1, max=5, divisions=4) 
-
-
+        self.opinion1.padding = ft.padding.only(left=256, bottom=50)
+        self.inst2 = ft.Text('自然さに関して次の5段階で評価してください。', 
+                            color=ft.colors.GREY_50)
+        self.inst2.padding=ft.padding.only(top=50)
+        self.opinion2.padding = ft.padding.only(left=256, bottom=50)
         self.nextb = ft.ElevatedButton(content=ft.Text('次へ'), 
                                        on_click=self.clicked,
                                        disabled=True)
@@ -283,33 +163,87 @@ class ViewBase(ft.View):
 
         self.key = key
         self.path = path
+        self.value1 = self.value2 = None
         self.start_time = None
-        self.end_time = None
+        self.end_time1 = self.end_time2 = None
 
+    def setup(self, src):
+        audio = ft.Audio(src=src, volume=1, balance=0, 
+                         on_state_changed=self.state_changed,
+                         on_loaded=self.activate_play_button,
+                         autoplay=False)
+        self.radio1_1 = ft.Radio(value="5", label="よくわかる", disabled=True )
+        self.radio1_2 = ft.Radio(value="4", label="ときどきわからない語がある", disabled=True)
+        self.radio1_3 = ft.Radio(value="3", label="聞き手が話題を知っていればわかる", disabled=True)
+        self.radio1_4 = ft.Radio(value="2", label="ときどきわかる語がある", disabled=True)
+        self.radio1_5 = ft.Radio(value="1", label="全くの了解不能", disabled=True)
+        opinion1 = ft.Container(content=ft.RadioGroup(content=ft.Column([
+            self.radio1_1, self.radio1_2, self.radio1_3, self.radio1_4, self.radio1_5
+        ]), on_change=self.value_changed1))
+        self.radio2_1 = ft.Radio(value="5", label="全くの自然である（不自然な要素がない）", disabled=True )
+        self.radio2_2 = ft.Radio(value="4", label="やや不自然な要素がある", disabled=True)
+        self.radio2_3 = ft.Radio(value="3", label="明らかに不自然である", disabled=True)
+        self.radio2_4 = ft.Radio(value="2", label="顕著に不自然である", disabled=True)
+        self.radio2_5 = ft.Radio(value="1", label="全く不自然である（自然な要素がない）", disabled=True)
+        self.radio2_6 = ft.Radio(value="0", label="内容を理解できず，評価できない", disabled=True)
+        opinion2 = ft.Container(content=ft.RadioGroup(content=ft.Column([
+            self.radio2_1, self.radio2_2, self.radio2_3, self.radio2_4, self.radio2_5, self.radio2_6
+        ]), on_change=self.value_changed2))
+        return audio, opinion1, opinion2
     
     def state_changed(self, e):
         if e.data == 'completed':
-            self.playb.disabled=False
-            self.playb.update()
-            self.nextb.disabled=False
-            self.nextb.update()
-        elif e.data == 'playing':
-            self.playb.disabled=True
-            self.playb.update()
-            self.nextb.disabled=True
-            self.nextb.update()
+            self.inst1.color=ft.colors.BLACK
+            self.inst1.update()
+            self.inst2.color=ft.colors.BLACK
+            self.inst2.update()
+            self.radio1_1.disabled=False
+            self.radio1_1.update()
+            self.radio1_2.disabled=False
+            self.radio1_2.update()
+            self.radio1_3.disabled=False
+            self.radio1_3.update()
+            self.radio1_4.disabled=False
+            self.radio1_4.update()
+            self.radio1_5.disabled=False
+            self.radio1_5.update()
+            self.radio2_1.disabled=False
+            self.radio2_1.update()
+            self.radio2_2.disabled=False
+            self.radio2_2.update()
+            self.radio2_3.disabled=False
+            self.radio2_3.update()
+            self.radio2_4.disabled=False
+            self.radio2_4.update()
+            self.radio2_5.disabled=False
+            self.radio2_5.update()
+            self.radio2_6.disabled=False
+            self.radio2_6.update()
 
     def clicked(self, e):
         raise NotImplementedError()
 
-    def value_changed(self, e):
-        raise NotImplementedError()
+    def value_changed1(self, e):
+        self.value1 = e.data
+        #self.nextm.color=ft.colors.BLACK
+        #self.nextm.update()
+        global jst_standard
+        self.end_time1 = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
+    
+    def value_changed2(self, e):
+        self.value2 = e.data
+        self.nextb.disabled = False
+        self.nextb.update()
+        self.nextm.color=ft.colors.BLACK
+        self.nextm.update()
+        global jst_standard
+        self.end_time2 = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
 
     def activate_play_button(self, e):
         self.playb.disabled=False
         self.playb.update()
 
-class TestView(ViewBase):
+class TestView(TestViewBase):
     def __init__(self, key, path):
         
         super().__init__(key, path)
@@ -322,16 +256,17 @@ class TestView(ViewBase):
             self.play_inst,
             self.playb,
             self.audio,
-            self.component1,
-            self.component2,
-            self.component3,
-            self.component4,
+            self.inst1,
+            self.opinion1,
+            self.inst2,
+            self.opinion2,
+            #self.reminder,
             self.nextm,
             self.nextb
         ]
 
         #time.sleep(1)
-        super(ViewBase, self).__init__("/testview", controls=controls)
+        super(TestViewBase, self).__init__("/testview", controls=controls)
         self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
         self.vertical_alignment = ft.MainAxisAlignment.CENTER
 
@@ -350,13 +285,11 @@ class TestView(ViewBase):
         global test_data_provider, eval_dict
         eval_dict['eval'].append('test')
         eval_dict['key'].append(self.key)
-        eval_dict['intelligiblity'].append(self.component1.get_value())
-        eval_dict['loudness'].append(self.component2.get_value())
-        eval_dict['intonation'].append(self.component3.get_value())
-        eval_dict['accent'].append(self.component4.get_value())
-
+        eval_dict['value1'].append(self.value1)
+        eval_dict['value2'].append(self.value2)
         eval_dict['start_time'].append(self.start_time)
-        eval_dict['end_time'].append(self.end_time)
+        eval_dict['end_time1'].append(self.end_time1)
+        eval_dict['end_time2'].append(self.end_time2)
         eval_dict['path'].append(test_data_provider.csv)
 
         try:
@@ -367,20 +300,11 @@ class TestView(ViewBase):
         except StopIteration:
             e.page.go('/lastview')
 
-    def value_changed(self, e):
-        #self.value = e.data
-        #self.nextb.disabled=False
-        #self.nextb.update()
-        self.nextm.color=ft.colors.BLACK
-        self.nextm.update()
-        global jst_standard
-        self.end_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3]
-    
-class TrialView(ViewBase):
+
+class TrialView(TestViewBase):
     def __init__(self, key, path):
         
         super().__init__(key, path)
-
         global trial_data_provider
         self.q_number = ft.Text("No." + str(trial_data_provider.counter) + " of " + str(trial_data_provider.__length__()))
         self.q_number.padding = ft.padding.only(bottom=100)
@@ -390,22 +314,24 @@ class TrialView(ViewBase):
                                 color=ft.colors.WHITE)
         self.add_inst2.padding=ft.padding.only(top=200)
 
+        #self.reminder = ft.Text("残り：" + str(trial_data_provider.remind()) + "/" + str(trial_data_provider.__length__()))
         controls = [
             self.q_number,
             self.add_inst1,
             self.play_inst,
             self.playb,
             self.audio,
-            self.component1,
-            self.component2,
-            self.component3,
-            self.component4,
             self.add_inst2,
+            self.inst1,
+            self.opinion1,
+            self.inst2,
+            self.opinion2,
+            #self.reminder,
             self.nextb
         ]
 
         #time.sleep(1)
-        super(ViewBase, self).__init__("/trialview", controls=controls)
+        super(TestViewBase, self).__init__("/trialview", controls=controls)
         self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
         self.vertical_alignment = ft.MainAxisAlignment.CENTER
 
@@ -413,9 +339,9 @@ class TrialView(ViewBase):
         super().state_changed(e)
         if e.data == 'completed':
             self.add_inst2.color=ft.colors.RED
-            #self.inst.color=ft.colors.BLACK
+            #self.add_inst1.color=ft.colors.BLACK
             self.add_inst2.update()
-            #self.inst.update()
+            #self.add_inst1.update()
             global jst_standard
             self.start_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S')
         elif e.data == 'playing':
@@ -426,13 +352,12 @@ class TrialView(ViewBase):
         global trial_data_provider, eval_dict
         eval_dict['eval'].append('trial')
         eval_dict['key'].append(self.key)
+        eval_dict['value1'].append(self.value1)
+        eval_dict['value2'].append(self.value2)
         eval_dict['start_time'].append(self.start_time)
-        eval_dict['end_time'].append(self.end_time)
+        eval_dict['end_time1'].append(self.end_time1)
+        eval_dict['end_time2'].append(self.end_time2)
         eval_dict['path'].append(trial_data_provider.csv)
-        eval_dict['intelligiblity'].append(self.component1.get_value())
-        eval_dict['loudness'].append(self.component2.get_value())
-        eval_dict['intonation'].append(self.component3.get_value())
-        eval_dict['accent'].append(self.component4.get_value())
 
         try:
             key, path = trial_data_provider.__next__()
@@ -442,28 +367,20 @@ class TrialView(ViewBase):
         except StopIteration:
             e.page.go('/intermview')
 
-    def value_changed(self, e):
-        self.nextb.disabled=False
-        self.nextb.update()
-        global jst_standard 
-        self.end_time = datetime.datetime.now(jst_standard).strftime('%Y/%m/%d %H:%M:%S')
-
 def main(page: ft.Page):
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_csv', type=str, required=True)
     parser.add_argument('--trial_csv', type=str, required=True)
-    #parser.add_argument('--speaker_csv', type=str, required=True)
     parser.add_argument('--output_csv', type=str, default="output.csv")
 
     args=parser.parse_args()
 
     global test_data_provider, trial_data_provider
-    #speaker_data_provider
     test_data_provider = DataProvider(args.test_csv, random=True)
-    trial_data_provider = DataProvider(args.trial_csv, random=True)
+    trial_data_provider = DataProvider(args.trial_csv)
     #speaker_data_provider = DataProvider(args.speaker_csv)
 
-    page.title="Speech Quality Test"
+    page.title="ディサーリア向け明瞭度/自然性聴取試験"
 
     pop_flag = False
 
@@ -482,9 +399,6 @@ def main(page: ft.Page):
             elif page.route == '/trialview':
                 key, path = trial_data_provider.__next__()
                 page.views.append(TrialView(key, path))
-            #elif page.route == '/speakerview':
-            #    key, path = speaker_data_provider.__next__()
-            #    page.views.append(SpeakerView(key, path))
             elif page.route == '/intermview':
                 page.views.append(IntermView())
             elif page.route == '/lastview':
